5331b7c2fb82f8842d603baeb2ac67ed09539f5e
Obsi-Elias <obsielias2019@gmail.com>
2023-05-09 21:10:23 +0300

    Single linked list inserting at front, end and any

--- single_linked_list_inserting.c
+++ single_linked_list_inserting.c
@@ -1,10 +1,13 @@
  #include <stdio.h>
  #include <stdlib.h>
- 
+void insertingAtFront(int data);
+void insertingAtEnd(int data);
+void insertingAtMiddle(int position, int data); 
  struct node {
         int data;
         struct node *link;
     }*head;
+
     
 int main()
 {
@@ -24,26 +27,55 @@ int main()
    head -> link -> link = current ;
 
    insertingAtFront(25);
+   insertingAtEnd(40);
+   insertingAtMiddle(3, 5);
 
 
    return 0;
 }
-
+// inserting node at the first position of the list
 void insertingAtFront(int data) {
 
-    struct node *newNodeEnd = NULL;
-    newNodeEnd = (struct node*) malloc(sizeof(struct node));
-    newNodeEnd -> data = data;
-    newNode -> link = head;
+    struct node *newNodeFront = NULL;
+    newNodeFront = (struct node*) malloc(sizeof(struct node));
+    newNodeFront -> data = data;
+    newNodeFront -> link = head;
     
-    head = newNode;
+    head = newNodeFront;
     printf("The new node is added to the field\n");
-    printf("The new node data value is: %d", head->data);
+    printf("The new node data value is: %d\n", head->data);
 }
+// Inserting node at the end of the node
 void insertingAtEnd(int data) {
     struct node *newNodeEnd = NULL;
     newNodeEnd = (struct node*) malloc(sizeof(struct node));
     newNodeEnd -> data = data;
     newNodeEnd -> link = NULL;
+    // Now traverse through the node 
+
+    struct node *ptr;
+    ptr = (struct node*) malloc(sizeof(struct node));
+    ptr = head;
+    while (ptr->link != NULL)
+    {
+        ptr -> link = ptr;
+    }
+    ptr -> link = newNodeEnd;
+    printf("Successfuly inserted node at the end\n");
+    printf("The new node data added at the end of the node is: %d \n", newNodeEnd -> data);
+}
+// Inserting node at any position of the list 
+void insertingAtMiddle(int position, int data) {
+    struct node *newNodeMiddle, *ptr;
+    newNodeMiddle = (struct node*) malloc(sizeof(struct node));
+    newNodeMiddle -> data = data;
+    newNodeMiddle -> link = NULL;
+    ptr = head;
+    for(int i = 1; i < position-1; i++) {
+        ptr = ptr -> link;
+    }
+    newNodeMiddle -> link = ptr -> link;
+    ptr -> link = newNodeMiddle;
+    printf("Successfully added a node at position %d", position);
     
 }
